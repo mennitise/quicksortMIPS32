e17bb28d9cddbfea1e7512cc16a0fefad8f58c86
mennitise <mennitise@gmail.com>
2018-10-10 16:20:42 -0300

    Start assembly implementation

--- main.c
+++ main.c
@@ -8,7 +8,8 @@
 
 const char* VERSION = "1.0.0\n";
 const char* HELP_MESSAGE = "Usage:\n	qsort -h\n	qsort -V\n	qsort [options] archivo\nOptions:\n	-h, --help Imprime ayuda.\n	-V, --version Versión del programa.\n	-o, --output\nArchivo de salida.\n	-n, --numeric Ordenar los datos numéricamente en vez de alfabéticamente.\nExamples:\n	qsort -n numeros.txt\n";
-const int MAX_LENGHT = 2048;
+const int MAX_LENGHT = 256;
+const int MAX_LENGHT_LINES = 256;
 
 static struct option long_options[] = {
 	{"help", 0, NULL, 'h'},
@@ -20,7 +21,9 @@ static struct option long_options[] = {
 
 /*----------------------------------QUICKSORT IMPLEMENTATION------------------------------------*/
 
-void qs(char list[MAX_LENGHT][MAX_LENGHT], int limit_left, int limit_right, int (*compare)(const void *, const void*)) {
+extern void qsortMIPS(char** izq, char** der, int num);
+
+void qs(char** list, int limit_left, int limit_right, int (*compare)(const void *, const void*)) {
 	int left, right;
 	char temp[MAX_LENGHT];
 	char pivot[MAX_LENGHT];
@@ -56,7 +59,7 @@ void qs(char list[MAX_LENGHT][MAX_LENGHT], int limit_left, int limit_right, int 
 	}
 }
 
-void quicksort(char list[MAX_LENGHT][MAX_LENGHT], int len, int (*compare)(const void *, const void*)) {
+void quicksort(char** list, int len, int (*compare)(const void *, const void*)) {
 	qs(list, 0, len-1, compare);
 }
 
@@ -107,25 +110,37 @@ void read_parameters(int argc, char *argv[], bool *numeric_order) {
 	}
 }
 
-void print_list(char list[MAX_LENGHT][MAX_LENGHT], size_t len){
-	for (int i = 0; i < len; ++i) {
+void print_list(char** list, size_t len){
+	int i;
+	for (i = 0; i < len; ++i) {
 		printf ("%s", list[i]);
 	}
 }
 
+void free_list(char** list, size_t len){
+	int i;
+	for (i = 0; i < len; ++i) {
+		free(list[i]);
+	}
+	free(list);
+}
+
 void sort_file(char* filename, bool numeric_order) {
-	char chars[1024];
+	char chars[MAX_LENGHT];
 	FILE *file = fopen(filename,"r");
 
-	char lines[MAX_LENGHT][MAX_LENGHT];
 	int lenLines = 0;
+	char **lines;
+	lines = malloc(MAX_LENGHT_LINES * sizeof(char*));
 
 	if (file == NULL){
+		free(lines);
 		printf("No such file\n");
 		exit(1);
 	} else {
 		while (feof(file) == 0) {
 			fgets(chars,1024,file);
+			lines[lenLines] = malloc(MAX_LENGHT*sizeof(char));
 			strcpy(lines[lenLines], chars);
 			lenLines++;
 		}
@@ -139,11 +154,37 @@ void sort_file(char* filename, bool numeric_order) {
 		quicksort(lines, lenLines, compare_str);
 	}
 	print_list(lines, lenLines);
+
+	free_list(lines, lenLines+1);
 }
 
 int main(int argc, char *argv[]) { 
+
+	// ----- TESTING ASSEMBLY ---------
+	char** tests;
+	tests = malloc(10 * sizeof(char*));
+	tests[0] = malloc(10 * sizeof(char));
+	strcpy(tests[0], "zero\n");
+	tests[1] = malloc(10 * sizeof(char));
+	strcpy(tests[1], "one\n");
+	tests[2] = malloc(10 * sizeof(char));
+	strcpy(tests[2], "two\n");
+	tests[3] = malloc(10 * sizeof(char));
+	strcpy(tests[3], "three\n");
+	tests[4] = malloc(10 * sizeof(char));
+	strcpy(tests[4], "four\n");
+	tests[5] = malloc(10 * sizeof(char));
+	strcpy(tests[5], "five\n");
+
+	print_list(tests, 6);
+	printf("-------------\n");
+	qsortMIPS(&tests[0], &tests[5], 0);
+	print_list(tests, 6);
+	// ---------------------------------
+
+
 	bool numeric_order = false; /* FLAG: numeric */
 	read_parameters(argc, argv, &numeric_order);
 	sort_file(argv[argc-1], numeric_order);
-	return 0; 
+	return 0;
 }
--- makefile
+++ makefile
@@ -1,6 +1,10 @@
 CC = gcc
-CFLAGS = -Wall
+CFLAGS = -Wall -g
 PROG = qsort
+MIPSFLAGS = quicksort.S
+
+.mips:
+	$(CC) $(CFLAGS) $(MIPSFLAGS) main.c -o $(PROG)
 
 all:
 	$(CC) $(CFLAGS) main.c -o $(PROG)
